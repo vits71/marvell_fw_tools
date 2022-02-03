ba25daf81dc7452dbe0f3f2cc7990ae4b0a5464f
vits71 <vits71@gmail.com>
2022-02-03 14:01:04 +0100

    added get_chunk func

--- decode.py
+++ decode.py
@@ -2,66 +2,72 @@
 
 '''
 Marvell binary firmware tool
-
-written by vit_sembera@trendmicro.com
 '''
 
 import sys, os, argparse, struct, crcmod
 from stat import *
 
+class ChunkCrcError(Exception):
+  pass
+  
+class UnknownChunkTypeError(Exception):
+  pass
+
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-v', '--verbose', action = 'count', default = 0, help='verbose level')
 parser.add_argument('infile', type=argparse.FileType('rb'))
-parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'))
 args = parser.parse_args()
 
 chunk_hdr_format = '<iiI'
 chunk_hdr_crc_format = '>I'
 chunk_data_crc_format = '>I'
-
 marvell_crc32 = crcmod.mkCrcFun(poly=0x104c11db7, initCrc=0, rev=False)
   
-  
-chunk_counter = 0
-total_len = 0
-final_chunk = False;
-crc_error = False
-output_data = b''
-      
-while not final_chunk and not crc_error:
+def get_chunk():
   chunk_hdr = args.infile.read(struct.calcsize(chunk_hdr_format))
   chunk_hdr_crc = struct.unpack(chunk_hdr_crc_format, args.infile.read(struct.calcsize(chunk_hdr_crc_format)))[0]
-  for ctype, addr, clen in struct.iter_unpack(chunk_hdr_format, chunk_hdr): 
+  for chunk_type, chunk_addr, chunk_len in struct.iter_unpack(chunk_hdr_format, chunk_hdr): 
     crc_hdr_calc = marvell_crc32(chunk_hdr)    
     if args.verbose > 1:
       print('Read {} bytes of chunk header.'.format(struct.calcsize(chunk_hdr_format)))
     if args.verbose > 0:
-      print('Chunk Type: {}, Addr: {:x}, Len: {}'.format(ctype, addr, clen))
+      print('Chunk Type: {}, Addr: {:#x}, Len: {}'.format(chunk_type, chunk_addr, chunk_len))
     if args.verbose > 1:
-      print('Chunk header CRC: {:x}, calculated CRC: {:x}'.format(chunk_hdr_crc, crc_hdr_calc))
-    if ctype == 1:
-      data = args.infile.read(clen - 4);
+      print('Chunk header CRC: {:#x}, calculated CRC: {:#x}'.format(chunk_hdr_crc, crc_hdr_calc))
+    if chunk_type == 1:
+      chunk_data = args.infile.read(chunk_len - struct.calcsize(chunk_data_crc_format));
       if args.verbose > 2:
         print('Raw header: {}'.format(chunk_hdr))
-        print('Raw data: {}'.format(data))
-      crc_data_calc = marvell_crc32(data)
+        print('Raw data: {}'.format(chunk_data))
+      crc_data_calc = marvell_crc32(chunk_data)
       chunk_data_crc = struct.unpack(chunk_data_crc_format, args.infile.read(struct.calcsize(chunk_data_crc_format)))[0]
       if args.verbose > 1:
-        print('Read {} bytes of chunk data. Chunk CRC: {:x}, calculated CRC: {:x}'.format(len(data), chunk_data_crc, crc_data_calc))               
-      chunk_counter += 1
-      total_len += clen - 4
-      output_data += data
-      crc_error = chunk_hdr_crc != crc_hdr_calc or chunk_data_crc != crc_data_calc
-    elif ctype == 4:
-      final_chunk = True
+        print('Read {} bytes of chunk data. Chunk CRC: {:#x}, calculated CRC: {:#x}'.format(len(chunk_data), chunk_data_crc, crc_data_calc))
+      if chunk_hdr_crc != crc_hdr_calc or chunk_data_crc != crc_data_calc:
+        raise ChunkCrcError(chunk_addr)  
+    elif chunk_type == 4:
+      chunk_data = None
     else:
-      print('Unknown chunk type {}'.format(ctype))
-if not crc_error:
-  print('Number of chunks: {}, Total length: {}'.format(chunk_counter, total_len))
-  if args.outfile:
-    written = args.outfile.write(output_data)
-    args.outfile.close()
-    print('Written {} bytes'.format(written))
-else:
-  print('CRC Error')    
-   
+      raise UnknownChunkTypeError(chunk_type)   
+    return (chunk_addr, chunk_data)
+
+
+
+  
+chunk_counter = 0
+output_data = b''
+      
+while True:
+  try:
+    chunk_addr, chunk_data = get_chunk()
+    if chunk_data is None:
+      break
+    chunk_counter += 1
+    output_data += chunk_data 
+  except ChunkCrcError as e:
+    print('CRC32 Error in chunk {}, address {:#x}'.format(chunk_counter, e.args[0]))
+    break
+  except UnknownChunkTypeError as e:
+    print('Unknown chunk type {}'.format(e.args[0]))
+    break
+print('Number of chunks: {}, Total length: {}'.format(chunk_counter, len(output_data)))
