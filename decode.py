06b2d6540ddf759780c337a0958ceb73d89be9ae
vits71 <vits71@gmail.com>
2022-02-09 16:37:44 +0100

    modify version

--- decode.py
+++ decode.py
@@ -4,7 +4,7 @@
 Marvell binary firmware tool
 '''
 
-import sys, os, argparse, struct, crcmod
+import sys, os, argparse, struct, crcmod, ast
 from stat import *
 
 class ChunkCrcError(Exception):
@@ -13,16 +13,20 @@ class ChunkCrcError(Exception):
 class UnknownChunkTypeError(Exception):
   pass
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('-v', '--verbose', action = 'count', default = 0, help='verbose level')
-parser.add_argument('infile', type=argparse.FileType('rb'))
-parser.add_argument('outfile', type=argparse.FileType('wb'))
-args = parser.parse_args()
+class ChunkWriteError(Exception):
+  pass
 
+def bytes_eval(b):
+  return ast.literal_eval(b)
+
+parser = argparse.ArgumentParser(description='Tool for manipulation with Marvell SoC firmware files')
+parser.add_argument('-v', '--verbose', action = 'count', default = 0, help='verbose level')
+subparsers = parser.add_subparsers(help='sub-command help')
+  
 chunk_hdr_format = '<iiI'
 chunk_hdr_crc_format = '>I'
 chunk_data_crc_format = '>I'
-marvell_crc32 = crcmod.mkCrcFun(poly=0x104c11db7, initCrc=0, rev=False)
+marvell_crc32 = crcmod.mkCrcFun(poly=0x104c11db7, initCrc=0, rev=False)  
   
 def read_chunk():
   chunk_hdr = args.infile.read(struct.calcsize(chunk_hdr_format))
@@ -62,32 +66,96 @@ def write_chunk(chunk_addr, chunk_data):
   if chunk_data is not None:
     chunk += chunk_data
     chunk += struct.pack(chunk_data_crc_format, marvell_crc32(chunk_data))
-  if args.verbose > 2:
-    print('Created chunk with data {}, len {} bytes'.format(chunk, len(chunk)))
+    if args.verbose > 2:
+      print('Created chunk data {}, len {} bytes'.format(chunk_data, len(chunk_data)))
   written = args.outfile.write(chunk)
   if written != len(chunk):
-    print('Error writing chunk. chunk length {}, written {}'.format(len(chunk), written)) 
+    raise ChunkWriteError(chunk_addr) 
   return written
   
-chunk_counter = 0
-chunk_list = []
-output_bytes = 0
-      
-while True:
-  try:
-    chunk_addr, chunk_data = read_chunk()
-    if chunk_data is None:
-      output_bytes += write_chunk(0, None)
+def copy():
+  chunk_counter = 0
+  output_bytes = 0      
+  while chunk_counter < args.count if args.count is not None else True:
+    try:
+      chunk_addr, chunk_data = read_chunk()
+      if chunk_data is None:
+        break
+      chunk_counter += 1
+      output_bytes += write_chunk(chunk_addr, chunk_data)
+    except ChunkCrcError as e:
+      print('CRC32 Error in chunk {}, address {:#x}'.format(chunk_counter, e.args[0]))
       break
-    chunk_counter += 1
-    chunk_list.append((chunk_addr, chunk_data))
-    output_bytes += write_chunk(chunk_addr, chunk_data)
-  except ChunkCrcError as e:
-    print('CRC32 Error in chunk {}, address {:#x}'.format(chunk_counter, e.args[0]))
-    break
-  except UnknownChunkTypeError as e:
-    print('Unknown chunk type {}'.format(e.args[0]))
-    break
-print('Number of chunks: {}'.format(chunk_counter))
-print('Bytes written: {}'.format(output_bytes))
+    except UnknownChunkTypeError as e:
+      print('Unknown chunk type {}'.format(e.args[0]))
+      break
+    except ChunkWriteError as e:
+      print('Error while writing chunk address {}'.format(e.args[0]))
+      break    
+  output_bytes += write_chunk(0, None)
+  chunk_counter += 1
+  print('Number of chunks: {}'.format(chunk_counter))
+  print('Bytes written: {}'.format(output_bytes))
 
+parser_copy = subparsers.add_parser('copy', help = 'copy (and verify) firmware file')
+parser_copy.add_argument('-c', '--count', type = int, help='number of chunks to copy (excluding last chunk), no count means all chunks')
+parser_copy.add_argument('infile', type=argparse.FileType('rb'))
+parser_copy.add_argument('outfile', type=argparse.FileType('wb'))
+parser_copy.set_defaults(func=copy)
+
+def modify():
+  if len(args.address) != len(args.data):
+    print('Number of address and data parameters don\'t match')
+    return
+  chunk_counter = 0
+  output_bytes = 0
+  modified_bytes = 0      
+  while True:
+    try:
+      chunk_addr, chunk_data = read_chunk()
+      if chunk_data is None:
+        break
+      chunk_range = range(chunk_addr, chunk_addr+len(chunk_data))
+      for patch_address in args.address:
+        patch_index = args.address.index(patch_address)
+        patch_data = args.data[patch_index]
+        if patch_address in chunk_range:
+          offset1 = patch_address-chunk_addr
+          patch_len = len(patch_data)
+          if offset1 + patch_len > len(chunk_data):
+            patch_len = len(chunk_data)-offset1
+            args.data[patch_index] = patch_data[:patch_len] 
+            args.address.insert(patch_index+1, chunk_addr + len(chunk_data))
+            args.data.insert(patch_index+1, patch_data[patch_len:]) 
+            patch_data = args.data[patch_index]
+          chunk_data = chunk_data[:offset1]+ patch_data + chunk_data[offset1 + patch_len:]
+          if args.verbose > 2:
+            print('Patched data {}, at address {:#x}, len {} bytes'.format(patch_data, patch_address, len(patch_data)))
+          modified_bytes += patch_len
+      output_bytes += write_chunk(chunk_addr, chunk_data)         
+    except ChunkCrcError as e:
+      print('CRC32 Error in chunk {}, address {:#x}'.format(chunk_counter, e.args[0]))
+      break
+    except UnknownChunkTypeError as e:
+      print('Unknown chunk type {}'.format(e.args[0]))
+      break
+    except ChunkWriteError as e:
+      print('Error while writing chunk address {}'.format(e.args[0]))
+      break    
+  output_bytes += write_chunk(0, None)
+  chunk_counter += 1
+  print('Number of chunks: {}'.format(chunk_counter))
+  print('Bytes modified: {}'.format(modified_bytes))
+  print('Bytes written: {}'.format(output_bytes))
+  
+  
+parser_modify = subparsers.add_parser('modify', help = 'modify and save firmware file')
+parser_modify.add_argument('infile', type=argparse.FileType('rb'))
+parser_modify.add_argument('outfile', type=argparse.FileType('wb'))
+parser_modify.add_argument('-a', '--address', type = lambda x: int(x,0), action='append', required = True, help='modification address start (can use 0x for hex)')
+parser_modify.add_argument('-d', '--data', type = lambda x: ast.literal_eval(x), action='append', required = True, help='data to modify as bytearray (use b\'\')')
+parser_modify.set_defaults(func=modify)
+
+args = parser.parse_args()
+print(args)
+args.func()
